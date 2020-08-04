c5612b533c2187000a83ddf2432d55baf4c48fe7
vultureschoice <betazeroisnotmylover@gmail.com>
2020-08-03 17:53:24 -0700

    add first GET route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mockUserData = [
   {name:'Jill'}
 ]
 
-app.get('/users', function(req, res)){
+app.get('/users', function(req, res){
   res.json({
     success: true,
     message: 'successfully got users. Nice!',
